51325dffc2f381cc85614a7ddbeda16054e87ea9
Bordigon <gistavovg@gmail.com>
2025-09-16 12:47:01 -0400

    primer commit

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "bootstrap"
 import './styles/index.css'
   
 // components
-import Home from './js/components/Home.jsx';
+import Home from './js/Home.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
